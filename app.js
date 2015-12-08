9e6d8950f268078a4f5bb9b85aef55c6cdae52b4
Paul Nathan <nathanp1@tcnj.edu>
2015-12-08 12:52:08 -0500

    Simple angular website displaying text posts and a button to add new posts

---
+++ app.js
@@ -0,0 +1,14 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', ['$scope', function($scope){
+  $scope.test = 'Hello world';
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 12},
+    {title: 'post 3', upvotes: 2},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 3}
+  ];
+  $scope.addPost = function(){
+    $scope.posts.push({title: 'A new post!', upvotes: 0});
+  };
+}]);
